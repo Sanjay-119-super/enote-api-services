830bc63e4667215c0899ce8df7df1b0bd019186e
Sanjay Kumar <coderdevil39@gmail.com>
2025-06-27 15:49:35 +0530

    initial commit

---
+++ src/test/java/com/sanjay/enote/EnoteApIsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanjay.enote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnoteApIsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
